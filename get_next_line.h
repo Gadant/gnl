df2525eec57e911a186b09979e29fedfc9444b8e
Gregoire Adant <gadant@e-r5-p8.s19.be>
2018-10-16 14:06:04 +0200

    oups le .h

---
+++ get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef _GET_NEXT_LINE_
+#define _GET_NEXT_LINE_
+
+#define BUFF_SIZE 100
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include "libft/libft.h"
+
+#endif
+
